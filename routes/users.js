96f7235ce0e6d70226133f59e51a9d13eb6136b4
YassinMk <122708120+YassinMk@users.noreply.github.com>
2023-05-14 13:20:34 +0000

    new features

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,113 @@
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
 
-// Récupérer take utilisateurs à partir de la position skip
-router.get('/', (req, res) => {
-  const { take, skip } = req.query;
-  // Code pour récupérer les utilisateurs avec les paramètres spécifiés
+
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => {
+  try {
+    // Retrieve all users from the database using Prisma
+    const users = await prisma.user.findMany();
+
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to get users.' });
+  }
 });
 
-// Récupérer un utilisateur ayant l'id donné
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  // Code pour récupérer l'utilisateur avec l'id spécifié
+
+  try {
+    // Retrieve the user from the database using Prisma
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(id) },
+    });
+    console.log(user,"hellowww");
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to get the user.' });
+  }
 });
 
-// Ajouter un nouvel utilisateur
-router.post('/', (req, res) => {
-  const userData = req.body;
-  // Code pour ajouter un nouvel utilisateur à partir des données envoyées
+// Route handler for creating a new user
+router.post('/', async (req, res) => {
+  try {
+    const { nom, email, password, role } = req.body;
+
+    // Validate the request body (you can use a validation library like Joi or express-validator)
+
+    // Create a new user in the database using Prisma
+    const newUser = await prisma.user.create({
+      data: {
+        nom,
+        email,
+        password,
+        role,
+      },
+      include: {
+        articles: true,
+      },
+    });
+
+    // Return the created user with articles in the response
+    res.status(201).json(newUser);
+  } catch (error) {
+    // Handle any errors that occurred during the user creation
+    console.error(error);
+    res.status(500).json({ error: 'Failed to create a new user.' });
+  }
 });
 
-// Mettre à jour l'utilisateur
-router.patch('/', (req, res) => {
-  const updatedUserData = req.body;
-  // Code pour mettre à jour l'utilisateur avec les données envoyées
-});
-
-// Supprimer l'utilisateur ayant l'id donné
-router.delete('/:id', (req, res) => {
+// Route handler for deleting a user
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  // Code pour supprimer l'utilisateur avec l'id spécifié
+
+  try {
+    // Delete the user from the database using Prisma
+    const deletedUser = await prisma.user.delete({
+      where: { id: parseInt(id) },
+    });
+
+    res.json(deletedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to delete the user.' });
+  }
+});
+
+// Route handler for updating a user
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nom, email, password, role } = req.body;
+
+  try {
+    // Update the user in the database using Prisma
+    const updatedUser = await prisma.user.update({
+      where: { id: parseInt(id) },
+      data: {
+        nom,
+        email,
+        password,
+        role,
+      },
+      include: {
+        articles: true,
+      },
+    });
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to update the user.' });
+  }
 });
 
 module.exports = router;
-
