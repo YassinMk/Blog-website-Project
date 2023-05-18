026aa6ae0e715bc9911684caaac834e0461bb4aa
YassinMk <122708120+YassinMk@users.noreply.github.com>
2023-05-18 21:12:19 +0000

    new features

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
+const { verifyToken } = require("../middleware/protection");
+
 
 
 const prisma = new PrismaClient();
